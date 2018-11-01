253fdc8845a09a9d6641f735d2edaacceacff94f
Anirban Saha <sahaa182823@gmail.com>
2018-11-01 23:14:25 +0530

    commited

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ModalGallery from './Component/ModalGallery'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <ModalGallery />
+      </div>
+    );
+  }
+}
+
+export default App;
